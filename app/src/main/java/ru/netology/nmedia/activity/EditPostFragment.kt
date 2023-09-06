062f803206680c12209818fe625b01e8f1aadd3b
Roman Avanesyan <creker88@rambler.ru>
2023-09-06 17:55:21 +0300

    HW11: перешел от activity к fragment

---
+++ app/src/main/java/ru/netology/nmedia/activity/EditPostFragment.kt
@@ -0,0 +1,38 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia.databinding.FragmentNewPostBinding
+import ru.netology.nmedia.util.AndroidUtils.focusAndShowKeyboard
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class EditPostFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentNewPostBinding.inflate(layoutInflater)
+
+        val viewModel: PostViewModel by activityViewModels()
+
+        binding.edit.setText("123") // надо передать текст
+        binding.edit.focusAndShowKeyboard()
+
+        binding.ok.setOnClickListener {
+
+            if (binding.edit.text.isNullOrBlank()) {
+                val content = binding.edit.text.toString()
+                viewModel.changeContentAndSave(content)
+            }
+            findNavController().navigateUp()
+        }
+        return binding.root
+    }
+}
